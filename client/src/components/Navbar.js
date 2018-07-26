cc55cf01b6f2850eac9263f5c12b7824ea2a3e50
Udeji Francis <francis.udejiofficial@gmail.com>
2018-07-26 15:44:51 +0100

    Added navbar to app

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+class MyNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <div className="container">
+          <NavbarBrand href="/">GingerHead</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/create">Create</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/join">Join</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Account
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Login
+                  </DropdownItem>
+                  <DropdownItem>
+                    Register
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Donate
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
