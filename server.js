bc5da945e33db3465faf2a78e9b3be194b620746
Udeji Francis <francis.udejiofficial@gmail.com>
2018-07-26 15:12:50 +0100

    Initial commit, react and express setup complete

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io');
+
+// init app
+const app = express();
+// define port
+const PORT = 4000 || process.env.PORT;
+
+// get home route
+app.get('/', (req, res) => {
+  res.send('Hello from SERVER')
+})
+
+// listen on port
+app.listen(PORT, err => {
+  if(err) throw err;
+  console.log(`Kahoot Clone server listening on http://localhost:${PORT}`);
+})
